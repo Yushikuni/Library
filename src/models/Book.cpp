aab68185bbe7a02b7c9e4266648d8da87af2e397
Kvetuse Husakova <42646031+Yushikuni@users.noreply.github.com>
2025-07-18 14:14:32 +0200

    add structure for book

---
+++ src/models/Book.cpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+struct Book
+{
+	std::string bookName;
+	std::string authorName;
+	std::string bookGander;
+	int releaseDate;
+	int bookCountPages;
+	std::string timeReadingFrom;
+	std::string timeReadingTo;
+	std::string bookReviews;
+	std::string notes;
+};
